1b5b5d0b926ad98b18fb25e48cd5715d9d810668
nguyenthaison <nguyenthaison0201@gmail.com>
2023-12-25 21:06:25 +0700

    fix cpp file

--- main.cpp
+++ main.cpp
@@ -4,18 +4,20 @@
 #include <algorithm>
 using namespace std;
 
-void doicho(int a[2])
+void doicho(int arr[2]) // arr là array - mảng được truyền vào hàm
 {
-    int temp = a[0];
-    a[0] = a[1];
-    a[1] = temp;
+    int temp = arr[0];
+    arr[0] = arr[1];
+    arr[1] = temp;
 }
 
 int main() {
     int a[2] = {5, 10}; // mảng với 2 giá trị là 5 và 10
-   for (auto& it = a.begin(); it != a.end() ;it++ ){
-    
-   }
+
+    doicho(a);
+
+    cout<<"i = 0: "<<a[0]<<endl;
+    cout<<"i = 1: "<<a[1]<<endl;
 }
 
 
